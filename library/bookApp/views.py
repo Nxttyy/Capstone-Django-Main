080631f169e99f70f2924b69a3ef0e1e676db870
Nathnael Yirga <natty@localhost.localdomain>
2024-03-08 15:08:06 +0300

    Books as a homepage view with template

--- library/bookApp/views.py
+++ library/bookApp/views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-	return HttpResponse('<h1>Books</h1>')
+	return render(request, 'bookApp/home.html')
--- library/library/settings.py
+++ library/library/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'adminApp', 
-    'bookApp',
+    'bookApp.apps.BookappConfig',
 ]
 
 MIDDLEWARE = [
