41b52e17f6fec553e0ca0f982e3671eddde0bf99
Nathnael Yirga <natty@localhost.localdomain>
2024-03-11 18:15:47 +0300

    book-descriptioin page at '/<int:id>'

--- library/bookApp/urls.py
+++ library/bookApp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog-home'),
+    path('', views.home, name='book-home'),
+    path('<int:book_id>/', views.book_detail, name='book-detail'),
+
 ]
--- library/bookApp/views.py
+++ library/bookApp/views.py
@@ -9,3 +9,10 @@ def home(request):
 	books = models.Book.objects.all()
 	print(books)
 	return render(request, 'bookApp/home.html', {'books':books, 'media_dir':settings.MEDIA_URL})
+
+
+def book_detail(request, book_id):
+	# book_id = request.GET.get('book_id')
+	book = models.Book.objects.filter(id=book_id)
+	print(book)
+	return render(request, 'bookApp/book-detail.html', {'book':book})
